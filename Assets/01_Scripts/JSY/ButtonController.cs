40a1f4d1f531d116c94e6f39073e89d0faf66563
yogurt-31 <syjang070813@gmail.com>
2024-01-01 23:23:40 +0900

    feat : 판넬 다트윈
    ㅈㄱㄴ

--- Assets/01_Scripts/JSY/ButtonController.cs
+++ Assets/01_Scripts/JSY/ButtonController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class ButtonController : MonoBehaviour
 {
     [SerializeField] private GameObject _optionPanel;
+    [SerializeField] private Ease ease;
 
     private bool isPanel;
     public void Option_Button()
@@ -12,7 +15,7 @@ public class ButtonController : MonoBehaviour
         if (!isPanel)
         {
             isPanel = true;
-            _optionPanel.SetActive(true);
+            _optionPanel.transform.DOMoveX(5, 0.5f).SetEase(ease);
         }
         else
         {
@@ -22,7 +25,12 @@ public class ButtonController : MonoBehaviour
 
     public void Continue_Button()
     {
-        _optionPanel.SetActive(false);
+        _optionPanel.transform.DOMoveX(-800, 0.5f).SetEase(ease);
         isPanel = false;
+    }
+
+    public void ExitScene_Button()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 }
