9b39c3ef6911ad31877eb1b96be561b02b000db8
LEE SANG GYU <tkdrb062@gmail.com>
2024-01-01 22:16:18 +0900

    fix: EasyJson

--- Assets/01_Scripts/EasyJson/EasyToJson.cs
+++ Assets/01_Scripts/EasyJson/EasyToJson.cs
@@ -7,6 +7,7 @@ namespace EasyJson
 {
     public static class EasyToJson
     {
+        private static string localPath = Application.persistentDataPath + "/Json/";
         /**
          * <summary>
          * Json 파일로 저장
@@ -17,7 +18,13 @@ namespace EasyJson
          */
         public static void ToJson<T>(T obj, string jsonFileName, bool prettyPrint = false)
         {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
+            if (!Directory.Exists(localPath))
+            {
+                Debug.Log("폴더가 존재하지 않습니다.");
+                Debug.Log("폴더를 생성합니다.");
+                Directory.CreateDirectory(localPath);
+            }
+            string path = localPath + jsonFileName + ".json";
             string json = JsonUtility.ToJson(obj, prettyPrint);
             File.WriteAllText(path, json);
             Debug.Log(json);
@@ -32,7 +39,12 @@ namespace EasyJson
          */
         public static T FromJson<T>(string jsonFileName)
         {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
+            string path = localPath + jsonFileName + ".json";
+            if (!File.Exists(path))
+            {
+                Debug.Log("파일이 존재하지 않습니다.");
+                return default;
+            }
             string json = File.ReadAllText(path);
             T obj = JsonUtility.FromJson<T>(json);
             return obj;
@@ -48,7 +60,14 @@ namespace EasyJson
          */
         public static void ListToJson<T>(List<T> list, string jsonFileName, bool prettyPrint = false)
         {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
+            Debug.Log(localPath);
+            if (!Directory.Exists(localPath))
+            {
+                Debug.Log("폴더가 존재하지 않습니다.");
+                Debug.Log("폴더를 생성합니다.");
+                Directory.CreateDirectory(localPath);
+            }
+            string path = Path.Combine(localPath, jsonFileName + ".json");
             string json = JsonConvert.SerializeObject(list, prettyPrint ? Formatting.Indented : Formatting.None);
             File.WriteAllText(path, json);
             Debug.Log(json);
@@ -63,7 +82,7 @@ namespace EasyJson
          */
         public static List<T> ListFromJson<T>(string jsonFileName)
         {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
+            string path = Path.Combine(localPath, jsonFileName + ".json");
             string json = File.ReadAllText(path);
             List<T> obj = JsonConvert.DeserializeObject<List<T>>(json);
             return obj;
@@ -79,7 +98,13 @@ namespace EasyJson
          */
         public static void DictionaryToJson<T, U>(Dictionary<T, U> dictionary, string jsonFileName, bool prettyPrint = false)
         {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
+            if (!Directory.Exists(localPath))
+            {
+                Debug.Log("폴더가 존재하지 않습니다.");
+                Debug.Log("폴더를 생성합니다.");
+                Directory.CreateDirectory(localPath);
+            }
+            string path = localPath + jsonFileName + ".json";
             string json = JsonConvert.SerializeObject(dictionary, prettyPrint ? Formatting.Indented : Formatting.None);
             File.WriteAllText(path, json);
             Debug.Log(json);
@@ -94,41 +119,9 @@ namespace EasyJson
          */
         public static Dictionary<T, U> DictionaryFromJson<T, U>(string jsonFileName)
         {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
+            string path = localPath + jsonFileName + ".json";
             string json = File.ReadAllText(path);
             Dictionary<T, U> obj = JsonConvert.DeserializeObject<Dictionary<T, U>>(json);
-            Debug.Log(json);
-            return obj;
-        }
-
-        /**
-         * <summary>
-         * Queue를 Json 파일로 저장
-         * </summary>
-         * <param name="queue">Json으로 저장할 Stack</param>
-         * <param name="jsonFileName">Json 파일 이름</param>
-         * <param name="prettyPrint">Json을 보기 좋게 출력할 지 여부</param>
-         */
-        public static void QueueToJson<T>(Queue<T> queue, string jsonFileName, bool prettyPrint = false)
-        {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
-            string json = JsonConvert.SerializeObject(queue, prettyPrint ? Formatting.Indented : Formatting.None);
-            File.WriteAllText(path, json);
-            Debug.Log(json);
-        }
-
-        /**
-         * <summary>
-         * Json 파일을 읽어서 Queue로 반환
-         * </summary>
-         * <param name="jsonFileName">Json 파일 이름</param>
-         * <returns>Json 파일을 읽어서 만든 Stack</returns>
-         */
-        public static Queue<T> QueueFromJson<T>(string jsonFileName)
-        {
-            string path = Path.Combine(Application.dataPath, jsonFileName + ".json");
-            string json = File.ReadAllText(path);
-            Queue<T> obj = JsonConvert.DeserializeObject<Queue<T>>(json);
             Debug.Log(json);
             return obj;
         }
