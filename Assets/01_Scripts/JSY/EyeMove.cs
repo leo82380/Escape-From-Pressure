3477dcc6c9939d9a91677fcdfb0e35871e7d67e2
LEE SANG GYU <tkdrb062@gmail.com>
2024-01-04 01:46:16 +0900

    feat: PetDoor Animation

--- Assets/01_Scripts/JSY/EyeMove.cs
+++ Assets/01_Scripts/JSY/EyeMove.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 public class EyeMove : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private EyeType eyeType;
+    
+    private enum EyeType
+    {
+        glass,
+        drawer,
+        table
+    }
     private Vector3 lookDir;
 
     private void Awake()
@@ -16,25 +24,37 @@ public class EyeMove : MonoBehaviour
         lookDir = target.position - transform.position;
         lookDir.Normalize();
         var look = Quaternion.LookRotation(lookDir).eulerAngles;
-        print(look.x + " " + look.y);
+
+        switch (eyeType)
+        {
+            case EyeType.table:
+            #region table
+                if (look.x is > 0 and < 180 and > 3)
+                {
+                    look.x = 3;
+                }
+                else if (look.x is < 360 and > 180 and < 357)
+                {
+                    look.x = 357;
+                }
         
-        if (look.x is > 0 and < 180 and > 3)
-        {
-            look.x = 3;
-        }
-        else if (look.x is < 360 and > 180 and < 357)
-        {
-            look.x = 357;
+                if (look.y is > 0 and < 180 and > 6)
+                {
+                    look.y = 6;
+                }
+                else if (look.y is < 360 and > 180 and < 354)
+                {
+                    look.y = 354;
+                }
+            #endregion
+            break;
+            case EyeType.glass:
+                break;
+            case EyeType.drawer:
+                break;
+            
         }
         
-        if (look.y is > 0 and < 180 and > 6)
-        {
-            look.y = 6;
-        }
-        else if (look.y is < 360 and > 180 and < 354)
-        {
-            look.y = 354;
-        }
 
         transform.rotation = Quaternion.Euler(look);
     }
