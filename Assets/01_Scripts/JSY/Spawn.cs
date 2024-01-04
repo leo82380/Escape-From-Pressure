cc432329cd642931641232c9b7c5fa36922b59cb
yogurt-31 <syjang070813@gmail.com>
2024-01-04 13:49:21 +0900

    feat : 스테이지 4 생성
    ㅇㅇㅇㅇㅇㅇ

--- Assets/01_Scripts/JSY/Spawn.cs
+++ Assets/01_Scripts/JSY/Spawn.cs
@@ -1,5 +1,22 @@
+using System.Collections;
 using UnityEngine;
 public class Spawn : MonoBehaviour
 {
-    private void Awake() => FindObjectOfType<PlayerController>().transform.position = transform.position;
+    private void Awake()
+    {
+        FindObjectOfType<PlayerController>().transform.position = transform.position;
+        StartCoroutine(CoolTime());
+    }
+
+    IEnumerator CoolTime()
+    {
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(FindObjectOfType<TV_MaterialController>().WhileAnime());
+        for (int i = 0; i < 3; i++)
+        {
+            StartCoroutine(FindObjectsOfType<PictureMaterial>()[i].ChangeAllMaterial());
+        }
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(FindObjectOfType<LightOnOff>().OnOffLight());
+    }
 }
