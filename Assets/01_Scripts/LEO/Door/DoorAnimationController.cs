f23e51a2399bd8c2a69b3dca81f9de4c907523e1
yogurt-31 <syjang070813@gmail.com>
2024-01-04 20:18:55 +0900

    feat : 씬연결

--- Assets/01_Scripts/LEO/Door/DoorAnimationController.cs
+++ Assets/01_Scripts/LEO/Door/DoorAnimationController.cs
@@ -1,12 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorAnimationController : MonoSingleton<DoorAnimationController>
 {
+    [SerializeField] private GameObject dad;
     private Animator _animator;
 
     protected override void Awake()
     {
-        base.Awake();
         _animator = GetComponent<Animator>();
     }
     
@@ -17,7 +18,16 @@ public class DoorAnimationController : MonoSingleton<DoorAnimationController>
     
     public void StageTwoDoorOpen(bool isOpen)
     {
+        StartCoroutine(Cooltimeeeeee(isOpen));
+    }
+
+    IEnumerator Cooltimeeeeee(bool isOpen)
+    {
         _animator.SetBool("Stage2", isOpen);
+        yield return new WaitForSeconds(1f);
+        _animator.SetBool("Stage2", false);
+        yield return new WaitForSeconds(3f);
+        dad.SetActive(false);
     }
     
 }
